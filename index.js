9b48cb7352321aa024c800b0a7952679cd696bf2
Said571 <103311770+Said571@users.noreply.github.com>
2023-09-07 20:52:07 +0600

    4 files modified

--- index.js
+++ index.js
@@ -2,5 +2,27 @@
 import './style.css';
 
 // Write Javascript code!
-const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Starter</h1>`;
+const buttons = document.querySelectorAll('.button');
+const body = document.querySelector('body');
+const input = document.querySelector('.input');
+
+buttons.forEach(function (btn) {
+  btn.addEventListener('click', function (btn) {
+    if (btn.target.id === 'gray') {
+      body.style.backgroundColor = btn.target.id;
+      input.innerHTML = btn.target.id;
+    }
+    if (btn.target.id === 'white') {
+      body.style.backgroundColor = btn.target.id;
+      input.innerHTML = btn.target.id;
+    }
+    if (btn.target.id === 'yellow') {
+      body.style.backgroundColor = btn.target.id;
+      input.innerHTML = btn.target.id;
+    }
+    if (btn.target.id === 'blue') {
+      body.style.backgroundColor = btn.target.id;
+      input.I = btn.target.id;
+    }
+  });
+});
